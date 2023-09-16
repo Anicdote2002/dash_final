4d924329e53171e7805364274854eb1a5c7cac70
Anicdote2002 <aniruddh.chauhan.2002@gmail.com>
2023-09-16 11:32:56 -0900

    final commit changes. there may be loose gauge, you can easily discard that

--- index.js
+++ index.js
@@ -1,91 +1,3 @@
-
-// const {app, BrowserWindow, Menu, dialog} = require("electron");
-
-// const options = {message:'button working!'};
-
-// function createWindow() {
-//     // Create the browser window 
-//     // (height and width of NHD-5.0-HDMI-N-RTXL-RTU)
-//     win = new BrowserWindow({ 
-//         width: 800, 
-//         height: 480, 
-//      });
-     
-//     // set full screen (matters for the pi)
-//     // COMMENT THIS OUT IF RUNNING ON YOUR OWN MACHINE
-//     // OR USE ESCAPE TO TOGGLE BACK TO WINDOWED
-//    //win.setFullScreen(true);
-//     // and load the index.html of the app.
-//    //win.loadFile("index.html");
-//    win.loadFile("sample_index.html");
-// }
-
-// app.on("ready", createWindow);
-
-// const { app, BrowserWindow, dialog } = require("electron");
-
-// function createWindow() {
-//   // Create the browser window
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 480,
-//   });
-
-//   // Load the index.html file
-//   win.loadFile("sample_index.html");
-
-//   // Uncomment the following line if you want the window to open in fullscreen
-//   // win.setFullScreen(true);
-// }
-
-// app.whenReady().then(() => {
-//   createWindow();
-
-//   app.on("activate", function () {
-//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-//   });
-// });
-
-// app.on("window-all-closed", function () {
-//   if (process.platform !== "darwin") app.quit();
-// });
-
-// const { app, BrowserWindow, ipcMain } = require('electron')
-
-
-
-// function createWindow() {
-//   // Create the browser window
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 480,
-//     webPreferences: {
-//       nodeIntegration: true, // Enable Node.js integration
-//       contextIsolation: false,
-//       enableRemoteModule: true,
-//     },
-//   });
-
-//   // Load the index.html file
-//   //win.loadFile("sample_index.html");
-//   win.loadFile("index.html");
-
-//   // Uncomment the following line if you want the window to open in fullscreen
-//   // win.setFullScreen(true);
-// }
-
-// app.whenReady().then(() => {
-//   createWindow();
-
-//   app.on("activate", function () {
-//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-//   });
-// });
-
-// app.on("window-all-closed", function () {
-//   if (process.platform !== "darwin") app.quit();
-// });
-
 const { app, BrowserWindow, ipcMain } = require('electron');
 
 function createWindow() {
@@ -102,8 +14,10 @@ function createWindow() {
   });
 
   // Load the index.html file
-  //win.loadFile("sample_index.html");
-  win.loadFile("index.html");
+  win.loadFile("sample_index.html");
+  //win.loadFile("index.html");
+
+  
 
   // Uncomment the following line if you want the window to open in fullscreen
   // win.setFullScreen(true);
